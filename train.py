290700c027756c7850fd356995d5ad41566c9688
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2023-02-13 17:57:17 +0200

    Removing one extra param

--- train.py
+++ train.py
@@ -32,12 +32,21 @@ from model import GPTConfig, GPT
 from lora import lora_find_and_replace, mark_only_lora_as_trainable, gpt2_peft_config, print_trainable_parameters
 
 
-def construct_path_suffix(config: Dict, base_config: Dict, always_include: Optional[List[str]] = None) -> str:
+def construct_path_suffix(
+    config: Dict,
+    base_config: Dict,
+    always_include: Optional[List[str]] = None,
+    always_ignore: Optional[List[str]] = None,
+) -> str:
     suffix_parts: List[str] = []
     if always_include is None:
         always_include = []
+    if always_ignore is None:
+        always_ignore = []
 
     for k in sorted(config.keys()):
+        if k in always_ignore:
+            continue
         if k in always_include or config[k] != base_config.get(k):
             suffix_parts.append(f"{k}-{str(config[k]).replace('.', '_').replace('/', '_')}")
 
@@ -96,7 +105,7 @@ config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 
 
 ckpt_path = Path(ckpt_path)
-ckpt_suffix = construct_path_suffix(config, base_config, always_include=["init"])
+ckpt_suffix = construct_path_suffix(config, base_config, always_include=["init"], always_ignore=["ckpt_path"])
 ckpt_path = ckpt_path.parent / f"{ckpt_path.stem}-{ckpt_suffix}{ckpt_path.suffix}"
 print(f"Saving checkpoint to {ckpt_path}")
 # -----------------------------------------------------------------------------
