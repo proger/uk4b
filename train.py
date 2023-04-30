9c4c03fe2aadc7fd33df1569a551e7a27b0b4419
Vol <volodymyr.kyrylov@usi.ch>
2023-04-30 16:29:12 +0200

    back to causal LM training

--- train.py
+++ train.py
@@ -115,7 +115,8 @@ def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
-    x, y = mask_tokens(x)
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+    #x, y = mask_tokens(x)
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
